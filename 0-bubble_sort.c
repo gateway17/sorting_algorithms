aec822c394738bc666a79eab3af890990c77128e
sergiovelasquez18 <2458@holbertonschool.com>
2021-06-15 16:41:12 -0500

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,23 @@
+#include "sort.h"
+void bubble_sort(int *array, size_t size)
+{
+	int aux;
+	size_t i, j;
+
+	if (array == NULL || size == 0)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				aux = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = aux;
+				print_array(array, size);
+			}
+		}
+	}
+}
