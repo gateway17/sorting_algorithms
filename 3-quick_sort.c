967038441ee4e7d88aca0188cd5796cf54aeb1e1
Andres Sepulveda <holberton@NNRX2000>
2021-06-17 21:21:02 -0500

    Fix indentation.

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -66,7 +66,7 @@ void quick_s(int *array, int lo, int hi, size_t size)
 /**
  * quick_sort - sort array using quicksort algorithm
  *
- *  @array: Pointer to  the beginning of an array
+ * @array: Pointer to  the beginning of an array
  * @size: The size of the array
  * Return: index of pivot
  */
